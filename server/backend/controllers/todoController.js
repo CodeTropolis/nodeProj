54b79e4b7c2d92413afefb4d9e47330a5baf199b
Michael <michael@codetropolis.com>
2023-04-18 14:25:24 -0500

    C

--- server/backend/controllers/todoController.js
+++ server/backend/controllers/todoController.js
@@ -1,8 +1,14 @@
-const getTodos = (req, res) => {
+const asyncHanlder = require('express-async-handler');
+
+const getTodos = asyncHandler(async (req, res) => {
   res.status(200).json({message: 'Get todos'})
-}
+})
 
 const setTodo = (req, res) => {
+ if (!req.body.text) {
+    res.status(400);
+    throw new Error('Please add text field');
+  }
   res.status(200).json({message: 'Set todo'})
 }
 
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const port = 3000;
 const app = express();
+const {errorHandler} = require('./backend/middleware/errorMiddleware')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use('/api/todos', require('./backend/routes/todoRoutes'))
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
